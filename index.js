41f19770d9bcfeb7ab879cdd47f993846548ad7e
Daniel Guieb <35285976+danielaguieb@users.noreply.github.com>
2020-11-13 22:47:54 -0700

    Final clean up

--- index.js
+++ index.js
@@ -2,18 +2,11 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-// app.use(express.static('css'));
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// Debugging
-// let arst = function(){};
-let arst = console.log.bind(console);
-
-
-// make sure max size is 200
+// For maintaining the Queue of messages
 function enqueue(theQueue, theSocketID, theTimeStamp, theUser, theMsg, theColor) {
 	if(theQueue.length > 200)
 		theQueue.shift();
@@ -26,11 +19,13 @@ function enqueue(theQueue, theSocketID, theTimeStamp, theUser, theMsg, theColor)
 	return message;
 }
 
+// For formatting timestamp
 function getTimeStamp() {
 	let curr = new Date();
 	return "["+curr.getHours()+":"+curr.getMinutes()+"]";
 }
 
+// For checking if a username is taken
 function checkDuplicateName(u, name){
 	for(let key in u) {
 		if(u[key][0] === name)
@@ -39,6 +34,7 @@ function checkDuplicateName(u, name){
 	return false;
 }
 
+// For updating the messages with the color
 function updateMessagesColor(m, s_id, clr){
 	for(let i = 0; i<m.length; i++){
 		if(m[i].sock_id === s_id)
@@ -46,6 +42,7 @@ function updateMessagesColor(m, s_id, clr){
 	}
 }
 
+// For validating RRGGBB value where each character is 0-9a-fA-F
 function isRRGGBB(clr){
 	if(clr.length === 6){
 		for(let i=0; i<clr.length; i++){
@@ -58,17 +55,20 @@ function isRRGGBB(clr){
 	return false;
 }
 
+// For replacing text with emojis
 function replaceWithEmojis(msg){
 	return msg.replace(":)", String.fromCodePoint(0x1F600)
 		).replace(":(", String.fromCodePoint(0x1F641)
 		).replace(":O", String.fromCodePoint(0x1F632));
 }
 
+
+// Globabl variables for list of users and list of messages
+
 /*
  * socketid: [username, color]
 */
 var users = {};
-
 
 /*
  * sock_id: theSocketID,
@@ -81,57 +81,88 @@ var messages = [];
 
 io.on('connect', (socket) => {
 	let socketid = socket.id;
-	// sends socket id to client, creating their generated userName using it
+
+	// Send socketID to client
 	socket.emit('setUserID', socketid);
+
+	// Load in chat log history to new client
 	for(let i=0; i<messages.length; i++){
 		io.to(socketid).emit('chat message', messages[i]);
 	}
 
+
+	// Welcome toast message
 	io.to(socketid).emit('toast message', "Welcome. You are User"+socketid+"!");
 
+
+	// Send socket ID and username
 	users[socketid] = ["User"+socketid, "000000"];
 	io.emit('updateUserList', users);
-	arst(users);
 
+
+	// On a client disconnecting, remove user from the users list
 	socket.on('disconnect', () => {
 		delete users[socketid];
 		io.emit('updateUserList', users);
 	});
 
 
-	// when a chat message is received, emit it to all sockets
+	// On recieving chat message, send to all clients
 	socket.on('chat message', (msg) => {
 		if(msg.charAt(0) !== "/"){
+
+			// Emoji text replacement
 			let emojiMsg = replaceWithEmojis(msg);
+
+			// Emit to all
 			io.emit('chat message', 
 				enqueue(messages, socketid, getTimeStamp(), users[socketid][0], emojiMsg, users[socketid][1]));
 		}
+
+		// Command handling
 		else {
 			let command = msg.split(' ');
+
+			// Handle /name command
 			if(command[0] === "/name"){
+
+				// Error handle duplicate name and toast message
 				if(checkDuplicateName(users, command[1])){
 					io.to(socketid).emit('toast message', "That username is already taken");
 				}
+
+				// Error handle name being too long and toast message
 				else if (command[1].length > 30){
 					io.to(socketid).emit('toast message', "Please enter a username under 30 characters");
 				}
+
+				// If fine, update name
 				else {
 					socket.emit('updateUserName', command[1]);
 					users[socketid][0] = command[1];
 					io.emit('updateUserList', users);
 				}
 			}
+
+			// Handle /color command
 			else if(command[0] === "/color") {
+
+				// Verify valid RRGGBB value
 				if(isRRGGBB(command[1])){
 					users[socketid][1] = command[1];
-					arst(users);
 					updateMessagesColor(messages, socketid, command[1]);
-					arst(messages);
 					io.emit('updateMessageColor', messages);
 				}
+
+				// Error handle invalid RRGGBB value
 				else {
 					io.to(socketid).emit('toast message', "Please input a RRGGBB format for your color");
 				}
+			}
+
+			// Error handle incorrect command
+			else {
+				io.to(socketid).emit('toast message', "Sorry that command does not exist");
 			}
 		}
 	});
